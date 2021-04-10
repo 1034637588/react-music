82b675dc09340de60b7bf88e91324f332cc51968
zhouLinyang <1034637588@qq.com>
2021-04-10 16:27:34 +0800

    使用 scss module

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,6 +1,6 @@
 import React, { memo } from 'react';
 
-import styles from './style.module.less';
+import styles from './style.module.scss';
 export default memo(function Header() {
     return (
         <div className={styles.HeaderWrapper}>
