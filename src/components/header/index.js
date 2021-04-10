f13b5d881473c0d3c0ff4bbfeda44cb7ccfc6e0c
zhouLinyang <1034637588@qq.com>
2021-04-10 17:23:11 +0800

    完成头部样式

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,10 +1,44 @@
 import React, { memo } from 'react';
+import { NavLink } from 'react-router-dom';
 
+import { headerLinks } from '@/common/local-data'
 import styles from './style.module.scss';
 export default memo(function Header() {
+    const showSelectItem = (item, index) => {
+        if (index < 3) {
+            return (
+                <NavLink activeClassName={styles.active} to={item.link} exact>
+                    {item.title}
+                    <i className={`${styles.icon} sprite_01`}></i>
+                </NavLink>
+            )
+        } else {
+            return <a href={item.link}>{item.title}</a>
+        }
+    }
     return (
         <div className={styles.HeaderWrapper}>
-            Header
+            <div className={`${styles.content} wrap-v1`}>
+                <div className={styles.HeaderLeft}>
+                    <a href="#/" className={`${styles.logo} sprite_01`}>网易云音乐</a>
+                    <div className={styles['select-list']}>
+                        {
+                            headerLinks.map((item, index) => {
+                                return (
+                                    <div key={item.title} className={styles['select-item']}>
+                                        {showSelectItem(item, index)}
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+                <div className={styles.HeaderRight}>
+                    <div className={styles.center}>创作者中心</div>
+                    <div>登录</div>
+                </div>
+            </div>
+            <div className={styles.divider}></div>
         </div>
     )
 })
