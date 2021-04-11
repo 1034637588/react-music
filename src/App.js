6f502c51ba4d76c2ceb4cf90af50bdce27db267e
zhouLinyang <1034637588@qq.com>
2021-04-11 13:25:15 +0800

    完成redux的基础配置

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { memo } from 'react';
 import { renderRoutes } from 'react-router-config';
 import { HashRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import routers from './router';
+import store from './store';
 
 import Footer from '@/components/footer';
 import Header from '@/components/header';
 
 export default memo(function App() {
   return (
-    <HashRouter>
-      <Header />
+    <Provider store={store}>
+      <HashRouter>
+        <Header />
         {renderRoutes(routers)}
-      <Footer />
-    </HashRouter>
+        <Footer />
+      </HashRouter>
+    </Provider>
   )
 })
