1667c92be2c3a817cda3a8704a8995ca225bd904
zhouLinyang <1034637588@qq.com>
2021-04-11 13:36:56 +0800

    添加recommend的 reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import { reducer as recommendReducer } from '../pages/discover/child-pages/recommend/store'
 
 const reducer = combineReducers({
-
+    recommendReducer
 });
 
 export default reducer;
